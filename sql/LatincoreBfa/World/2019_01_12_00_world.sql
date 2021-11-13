4565517ab8035bb7b59be51c632da1f8efb5b856
Legends of Azeroth <81566364+NemoPRM@users.noreply.github.com>
2021-11-13 13:16:15 +0500

    Create 2019_01_12_00_world.sql

---
+++ sql/LatincoreBfa/World/2019_01_12_00_world.sql
@@ -0,0 +1,9 @@
+8 lines (7 sloc)  284 Bytes
+  
+DROP TABLE IF EXISTS `creature_sparring_template`;
+CREATE TABLE `creature_sparring_template`(  
+  `AttackerEntry` MEDIUMINT(8) UNSIGNED NOT NULL,
+  `VictimEntry` MEDIUMINT(8) UNSIGNED NOT NULL,
+  `HealthLimitPct` FLOAT(5) DEFAULT 0,
+  PRIMARY KEY (`AttackerEntry`, `VictimEntry`)
+);
